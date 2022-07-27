d43b1be6b62b7c16c0861109327cd992300817ef
ZahidArman <21005622@myrp.edu.sg>
2022-07-27 14:34:12 +0800

    New Version

--- app/src/main/java/sg/edu/rp/c346/id21012050/custommovielist/ModifyActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id21012050/custommovielist/ModifyActivity.java
@@ -1,5 +1,6 @@
 package sg.edu.rp.c346.id21012050.custommovielist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
@@ -7,11 +8,15 @@ import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class ModifyActivity extends AppCompatActivity {
 
     EditText etID, etModTitle, etModGenre, etModYear;
     Spinner spnRatings;
     Button btnUpdate, btnDelete, btnCancel;
+    ArrayList spnItems;
+    int ratingIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,29 @@ public class ModifyActivity extends AppCompatActivity {
         btnUpdate = findViewById(R.id.buttonUpdate);
         btnDelete = findViewById(R.id.buttonDelete);
         btnCancel = findViewById(R.id.btnCancel);
+
+        spnItems.add("G");
+        spnItems.add("PG");
+        spnItems.add("PG13");
+        spnItems.add("NC16");
+        spnItems.add("M18");
+        spnItems.add("R21");
+
+
+        Intent i = getIntent();
+        final Movie currentMovie = (Movie) i.getSerializableExtra("data");
+
+        etID.setText(currentMovie.getId()+"");
+        etModTitle.setText(currentMovie.getTitle());
+        etModGenre.setText(currentMovie.getGenre());
+        etModYear.setText(currentMovie.getYear()+"");
+        for (int x = 0;x < spnItems.size();x++){
+            if (currentMovie.getRating() == spnItems.get(x)){
+                ratingIndex = x;
+            }
+        }
+        spnRatings.setSelection(ratingIndex);
+
 
     }
 }
