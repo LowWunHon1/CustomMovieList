29b43f3ea6000bfb0d26d0fff2868d77edcf7384
LowWunHon1 <21012050@myrp.edu.sg>
2022-07-27 11:07:30 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21012050/custommovielist/Movie.java
@@ -0,0 +1,12 @@
+package sg.edu.rp.c346.id21012050.custommovielist;
+
+import java.io.Serializable;
+
+public class Movie implements Serializable {
+
+    private String title;
+    private String genre;
+    private int year;
+    private String rating;
+
+}
